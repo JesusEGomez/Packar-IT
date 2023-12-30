5c7f524b158f8c7189115ad2614e9c9afda8a00e
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-12-30 17:27:26 -0300

    Login Back

---
+++ Front/next-auth-mongodb/src/app/api/auth/signup/route.ts
@@ -0,0 +1,47 @@
+import { NextResponse } from "next/server"
+import User from "@/models/user"
+import { connectDB } from "@/libs/mongodb"
+import bcrypt from "bcrypt"
+
+
+export async function POST(request: Request) {
+    await connectDB();
+    const { fullname, email, password } = await request.json();
+    console.log(fullname, email, password);
+
+    if (!fullname || !email || !password) {
+        return NextResponse.json({ message: "Todos los campos son obligatorios" });
+    }
+
+    if (password.length < 6) {
+        return NextResponse.json({ message: "La contraseña debe tener al menos 6 caracteres" }, { status: 400 });
+    }
+    try {
+        await connectDB();
+        const userFound = await User.findOne({ email });
+
+        if (userFound) {
+            return NextResponse.json({ message: "El usuario ya existe" }, { status: 400 });
+        }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        const user = new User({ fullname, email, password: hashedPassword });
+        const saveUser = await user.save();
+
+        console.log(saveUser);
+
+        return NextResponse.json(saveUser);
+    } catch (error) {
+
+        console.error(error);
+        if (error instanceof Error) {
+            return NextResponse.json({ message: error.message }, { status: 500 });
+        }
+        return NextResponse.json(
+            { message: "Internal Server Error" },
+            { status: 500 }
+        )
+    }
+}
+
