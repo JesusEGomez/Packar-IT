a868741c721470f549feb5f7503e020e13eb4e61
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-12-31 11:27:29 -0300

    Google Auth

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -7,7 +7,7 @@ const handler = NextAuth({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
         })
-    ]
+    ] 
 })
 
 export { handler as GET , handler as POST }
