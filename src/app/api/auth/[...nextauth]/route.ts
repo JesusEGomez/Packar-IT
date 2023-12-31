eba2f46563be857555382da26b840c2724f5540f
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2023-12-31 16:51:03 -0300

    Log In parte 1

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,27 @@
 import NextAuth from "next-auth"
 import GoogleProvider from "next-auth/providers/google"
+import CredentialsProvider from "next-auth/providers/credentials"
 
 const handler = NextAuth({
     providers: [
         GoogleProvider({
             clientId: process.env.GOOGLE_CLIENT_ID as string,
             clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
+        }),
+        CredentialsProvider({
+            name: "credentials",
+            credentials: {
+                email: { label: "Email", type: "email", placeholder: "email" },
+                password: { label: "Password", type: "password" },
+                async authorize(credentials, req) {
+                    
+                    return user
+                }
+            }
         })
-    ] 
+    ],
+
+
 })
 
-export { handler as GET , handler as POST }
+export { handler as GET, handler as POST }
