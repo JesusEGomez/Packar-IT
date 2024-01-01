6daa1382451b0caa5e62a1d9b36152705135159b
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2024-01-01 11:05:12 -0300

    Auth

---
+++ src/app/landing/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+
+export default function landing() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen bg-rose-600">
+      <div className="text-center">
+        <h2 className="text-azulprimario">Aprovecha el viaje de otras personas para enviar lo que necesites</h2>
+      </div>
+      <div>
+        <p>Boton 1</p>
+        <p>Boton 2</p>
+      </div>
+    </div>
+  );
+}
+
+// rgb(254, 19, 82) - rojo  rgb(24, 24, 28) - negro
