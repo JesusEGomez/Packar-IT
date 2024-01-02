2cc6a521c9c13452ad1afa2b4ee970e9031b2bd9
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2024-01-02 15:41:57 -0300

    Modelo de Productos

---
+++ src/app/api/auth/productos/route.ts
@@ -0,0 +1,38 @@
+import { NextResponse } from "next/server";
+import Producto from "@/models/productos";  
+import { connectDB } from "@/libs/mongodb";
+
+export async function POST(request: Request) {
+    await connectDB();
+    const { categoria, nombre, tamaño, peso } = await request.json();
+
+    if (!categoria || !nombre || !tamaño || !peso) {
+        return NextResponse.json({ message: "Todos los campos son obligatorios" });
+    }
+
+    try {
+        const producto = new Producto({ categoria, nombre, tamaño, peso });  
+        const savedProducto = await producto.save();  
+
+        console.log(savedProducto);
+
+        return NextResponse.json(savedProducto);
+    } catch (error) {
+        console.error(error);
+        if (error instanceof Error) {
+            return NextResponse.json({ message: error.message }, { status: 500 });
+        }
+        return NextResponse.json({ message: "Internal Server Error" }, { status: 500 });
+    }
+}
+
+export async function GET(request: Request) {
+    try {
+        await connectDB();
+        const productos = await Producto.find();  
+        return NextResponse.json(productos);  
+    } catch (error) {
+        console.error(error);
+        return NextResponse.json({ message: "Error al obtener los productos" });  
+    }
+}
---
+++ src/models/productos.ts
@@ -0,0 +1,26 @@
+import { Schema, models, model } from "mongoose";
+
+const ProductoSchema = new Schema({
+    categoria: {
+        type: String,
+        enum: ["Belleza", "Bebé", "Bricolaje y herramientas", "Deportes", "Electronica", "Equipaje"],
+        required: true,
+    },
+    nombre: {
+        type: String,
+        required: [true, "El nombre del producto es obligatorio"],
+    },
+    tamaño: {
+        type: String,
+        enum: ["64x30cm", "81x37cm", "67x44cm"],
+        required: true,
+    },
+    peso: {
+        type: String,
+        enum: ["< 5 Kg", "5-15 Kg", "15-30 Kg"],
+        required: true,
+    },
+});
+
+const Producto = models.Producto || model("Producto", ProductoSchema);
+export default Producto;
