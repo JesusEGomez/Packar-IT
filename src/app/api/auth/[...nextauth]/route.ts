9bcd2a7e261abdde144642cb1a089256616c896a
JesusEGomez <jesus.emanuel.gomez77@gmail.com>
2024-01-02 18:28:28 -0300

    fix repo

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,50 +1,51 @@
-import NextAuth from "next-auth"
-import GoogleProvider from "next-auth/providers/google"
-import CredentialsProvider from "next-auth/providers/credentials"
-import { connectDB } from "@/libs/mongodb"
-import User from "@/models/user"
-import bcrypt from "bcrypt"
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectDB } from "@/libs/mongodb";
+import User from "@/models/user";
+import bcrypt from "bcrypt";
+import { NextAuthOptions } from "next-auth";
 
+const options: NextAuthOptions = {
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+    CredentialsProvider({
+      name: "credentials",
+      credentials: {
+        email: { label: "Email", type: "email", placeholder: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        const email = credentials?.email;
+        const password = credentials?.password!;
+        try {
+          await connectDB();
+          const userFound = await User.findOne({ email }).select("+password");
+          if (!userFound) return null;
 
-const options = {
-    providers: [
-        GoogleProvider({
-            clientId: process.env.GOOGLE_CLIENT_ID as string,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET as string
-        }),
-        CredentialsProvider({
-            name: "credentials",
-            credentials: {
-                email: { label: "Email", type: "email", placeholder: "email" },
-                password: { label: "Password", type: "password" },
-                
-            },
-            async authorize(credentials, req) {
-                    const { email, password } = credentials;
-                try {
-                    await connectDB();
-                    const userFound = await User.findOne({ email }).select('+password');                    
-                    if (!userFound) return null;
-            
-                    const matchPassword = await bcrypt.compare(password, userFound.password);
-            
-                    if (!matchPassword) return null;
-            
-                    return {
-                        id: userFound._id,
-                        name: userFound.fullname,
-                        email: userFound.email,
-                    };
-                } catch (error) {
-                    console.log(error);
-                }
-            }
-        })
-    ],
+          const matchPassword = await bcrypt.compare(
+            password,
+            userFound.password
+          );
 
+          if (!matchPassword) return null;
 
-}
-const handler = NextAuth(options) 
+          return {
+            id: userFound._id,
+            name: userFound.fullname,
+            email: userFound.email,
+          };
+        } catch (error) {
+          console.log(error);
+          return null;
+        }
+      },
+    }),
+  ],
+};
+const handler = NextAuth(options);
 
-export { handler as GET, handler as POST }
-
+export { handler as GET, handler as POST };
--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,58 +1,63 @@
-import { NextResponse } from "next/server"
-import User from "@/models/user"
-import { connectDB } from "@/libs/mongodb"
-import bcrypt from "bcrypt"
-
+import { NextResponse } from "next/server";
+import User from "@/models/user";
+import { connectDB } from "@/libs/mongodb";
+import bcrypt from "bcrypt";
 
 export async function POST(request: Request) {
+  await connectDB();
+  const { fullname, email, password } = await request.json();
+  console.log(fullname, email, password);
+
+  if (!fullname || !email || !password) {
+    return NextResponse.json({ message: "Todos los campos son obligatorios" });
+  }
+
+  if (password.length < 6) {
+    return NextResponse.json(
+      { message: "La contraseña debe tener al menos 6 caracteres" },
+      { status: 400 }
+    );
+  }
+  try {
     await connectDB();
-    const { fullname, email, password } = await request.json();
-    console.log(fullname, email, password);
+    const userFound = await User.findOne({ email });
 
-    if (!fullname || !email || !password) {
-        return NextResponse.json({ message: "Todos los campos son obligatorios" });
+    if (userFound) {
+      return NextResponse.json(
+        { message: "El usuario ya existe" },
+        { status: 400 }
+      );
     }
 
-    if (password.length < 6) {
-        return NextResponse.json({ message: "La contraseña debe tener al menos 6 caracteres" }, { status: 400 });
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const user = new User({ fullname, email, password: hashedPassword });
+    const saveUser = await user.save();
+
+    console.log(saveUser);
+
+    return NextResponse.json(saveUser);
+  } catch (error) {
+    console.error(error);
+    if (error instanceof Error) {
+      return NextResponse.json({ message: error.message }, { status: 500 });
     }
-    try {
-        await connectDB();
-        const userFound = await User.findOne({ email });
-
-        if (userFound) {
-            return NextResponse.json({ message: "El usuario ya existe" }, { status: 400 });
-        }
-
-        const hashedPassword = await bcrypt.hash(password, 10);
-
-        const user = new User({ fullname, email, password: hashedPassword });
-        const saveUser = await user.save();
-
-        console.log(saveUser);        
-        
-        return NextResponse.json(saveUser);
-    } catch (error) {
-
-        console.error(error);
-        if (error instanceof Error) {
-            return NextResponse.json({ message: error.message }, { status: 500 });
-        }
-        return NextResponse.json(
-            { message: "Internal Server Error" },
-            { status: 500 }
-        )
-    }
+    return NextResponse.json(
+      { message: "Internal Server Error" },
+      { status: 500 }
+    );
+  }
 }
-
-
 
 export async function GET(request: Request) {
   try {
-    const users = await User.find(); 
+    const users = await User.find();
     return NextResponse.json(users);
   } catch (error) {
     console.error(error);
-    return NextResponse.json({ message: "Error al obtener los usuarios" }, { status: 500 });
+    return NextResponse.json(
+      { message: "Error al obtener los usuarios" },
+      { status: 500 }
+    );
   }
 }
