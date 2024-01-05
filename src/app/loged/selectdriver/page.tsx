bc5bc3ac48beda2b48e2f2be393576f98f9755d3
gealtach <cleivaj93@gmail.com>
2024-01-05 15:59:20 -0300

    go

--- src/app/loged/selectdriver/page.tsx
+++ src/app/loged/selectdriver/page.tsx
@@ -1,7 +1,7 @@
 'use client'
 import React, { useEffect, useState } from 'react'
 
-function page() {
+function Page() {
   const [viajes, setViajes] = useState<[] | null>(null);
   useEffect(() => {
     const fetchData = async () => {
@@ -26,4 +26,4 @@ function page() {
   )
 }
 
-export default page
+export default Page
