bd259f7cb17bd9f6baf30e3640fbf40b642642cf
gealtach <cleivaj93@gmail.com>
2024-01-05 23:29:57 -0300

    asd

--- src/app/misenvios/page.tsx
+++ src/app/misenvios/page.tsx
@@ -1,9 +1,9 @@
 'use client'
 import React, { useState } from 'react';
-import img from '../../../img/undraw_Happy_feeling_re_e76r.png';
-import { FaArrowLeft } from 'react-icons/fa';
+import img from '../../img/undraw_Happy_feeling_re_e76r.png';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
+import { FaArrowLeft } from 'react-icons/fa';
 import { GiPathDistance } from 'react-icons/gi';
 
 type Viaje = {
@@ -54,64 +54,7 @@ const MisEnvios: React.FC = () => {
 
   return (
     <div className='flex flex-col'>
-      <div className='m-10'>
-        <button onClick={() => navigate.push('/loged')} className='flex items-center'>
-          <FaArrowLeft className='mr-2' />
-        </button>
-      </div>
-      <div className='flex flex-col p-4 items-center'>
-        <h1 className='text-2xl font-bold'>Mis Envíos</h1>
-        <div className='border p-5'>box</div>
-        <div>
-          <Image src={img} width={250} height={250} alt='logo' />
-        </div>
-        <div className='flex flex-col items-center p-5'>
-          <h1 className='text-2xl'>Oops! Nada por acá!</h1>
-          <br />
-          <p className='text-center m-2'>
-            No encontramos trayectos con las características que indicaste.
-          </p>
-        </div>
-        <button
-          onClick={() => navigate.push('/loged')}
-          className='flex w-full mt-20 justify-center bg-pink p-3 rounded text-white'
-        >
-          Volver
-        </button>
-      </div>
-      {viajesDisponibles.length > 0 && (
-        <div className='flex flex-col w-full m-2 p-4 gap-y-3'>
-          <h1 className='text-xl'>Mis Envíos Disponibles</h1>
-          {viajesDisponibles.map((viaje, index) => (
-            <div className='flex flex-col border rounded shadow-lg p-4' key={index}>
-              <div className='flex w-full justify-between px-4'>
-                <div className='flex items-center'>
-                  <div className='w-12 h-12 bg-gray-300 rounded-full mr-2'></div>
-                  <p>{viaje.viajero.nombre}</p>
-                </div>
-                <p>{viaje.precio}€</p>
-              </div>
-              <div className='flex items-center mt-2'>
-                <GiPathDistance
-                  className={`${
-                    viaje.eresFlexible ? 'text-pink' : 'text-black'
-                  }`}
-                  size={50}
-                />
-                <div className='ml-2'>
-                  <p>
-                    Desde: {viaje.desde.lat} {viaje.horaSalida}
-                  </p>
-                  <p>
-                    Hasta: {viaje.hasta.lat} {viaje.horaLlegada}
-                  </p>
-                </div>
-              </div>
-              <div className='mt-2'>{viaje.cuando}</div>
-            </div>
-          ))}
-        </div>
-      )}
+      <h1 className='text-3xl'>Aqui Irian mis envios si tuviera!!</h1>
     </div>
   );
 };
