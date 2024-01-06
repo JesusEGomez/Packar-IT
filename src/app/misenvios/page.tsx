0510e9890524b00654faf78715f66f1a2f322250
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2024-01-06 16:25:20 -0300

    Vista cardenvios

--- src/app/misenvios/page.tsx
+++ src/app/misenvios/page.tsx
@@ -1,22 +1,24 @@
-"use client";
+import React from "react";
 import CardEnvios from "./cardenvios/psge";
 import { useRouter } from "next/navigation";
 
 const MisEnvios: React.FC = () => {
-  //const navigate = useRouter();
+  // const navigate = useRouter();
 
   return (
     <div className="flex flex-col justify-center items-center">
-    <div className="flex justify-center items-center gap-2 m-4 text-center sm:flex-col sm:items-start">
-      <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl m-1">
-        Mis Envíos
-      </h2>
-      <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl m-1">
-        Mis Trayectos
-      </h2>
-    </div>
-      <CardEnvios />
+      <div className="flex flex-col justify-center items-center text-center">
+        <div className="flex flex-row">
+          <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl m-1">
+            Mis Envíos
+          </h2>
+          <h2 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl xl:text-6xl m-1">
+            Mis Trayectos
+          </h2>
+        </div>
       </div>
+      <CardEnvios />
+    </div>
   );
 };
 
