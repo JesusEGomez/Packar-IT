65346466176e0d8c81640545a523e4abaa7310fa
JesusEGomez <jesus.emanuel.gomez77@gmail.com>
2024-01-17 20:16:24 -0300

    view mis viajes added

---
+++ src/app/loged/misenvios/viajes/page.tsx
@@ -0,0 +1,11 @@
+import TravelCard from "@/app/components/travelCard";
+
+const Viajes = () => {
+  return (
+    <div className="w-full flex justify-center items-center">
+      <TravelCard />
+    </div>
+  );
+};
+
+export default Viajes;
