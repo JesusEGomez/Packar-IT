19b9dd3c24b1cea6d16355288d8156fbfba3b378
gealtach <cleivaj93@gmail.com>
2024-01-16 23:11:23 -0300

    producto especial realizado

--- src/app/components/SpecialProdModal.tsx
+++ src/app/components/SpecialProdModal.tsx
@@ -1,23 +1,75 @@
 'use client'
 import { Button } from "@/components/ui/button";
+import { ChangeEvent, useEffect, useRef, useState } from "react";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { IoMdArrowRoundBack } from "react-icons/io";
+import { LuFolderInput } from "react-icons/lu";
 
 
 type FormInputs = {
     selected: string;
 }
 
-export default (props: any) => {
+const SpecialProdModal = (props: any) => {
+    const fileInputRef = useRef<HTMLInputElement>(null);
+    const [img, setImg] = useState<string | null>(null);
+    const [disabled, setDisable] = useState<boolean>(true);
+  
+    const handleFileChange = async (e: ChangeEvent<HTMLInputElement>) => {
+      const file = e.target.files?.[0];
+      if(file){
+        const formData = new FormData();
+        formData.append('file', file);
+        try {
+          const response = await fetch(`https://api.cloudinary.com/v1_1/dwj6wtgtb/image/upload?upload_preset=oxxsnr7q`,
+          {
+            method: "POST",
+            body: formData,
+          })
+          if(response.ok){
+            const ans = await response.json();
+            console.log(ans);
+            
+            setImg(ans.secure_url);
+          }
+        } catch (error) {
+          console.log(error);
+          
+        }
+      }
+    };
+
+    useEffect(() => {
+      img && setDisable(false);
+    },[img]);
+
+    const handleDivClick = () => {
+      if (fileInputRef.current) {
+        fileInputRef.current.click();
+      }
+    };
     const {
         register,
         handleSubmit,
         formState: { errors },
       } = useForm<FormInputs>();
       const onSubmit: SubmitHandler<FormInputs> = (data) => {
-        console.log({data, name: data});
-        props.closeFirstModal({data, name: data.selected});
-        props.closeModal(data);
+        props.closeFirstModal({
+          type: 'special',
+          name: data.selected,
+          size: 'special',
+          weigth: 'special',
+          photoProduct: img,
+          articulosEspeciales: data.selected
+        });
+        props.closeModal({
+          type: 'special',
+          name: data.selected,
+          size: 'special',
+          weigth: 'special',
+          photoProduct: img,
+          articulosEspeciales: data.selected
+        });
       };
       const close = () => {
         props.closeModal();
@@ -49,12 +101,28 @@ export default (props: any) => {
                 <option value="Esquis">Esquis</option>
                 <option value="Otro">Otro</option>
           </select>
+          <div className='flex flex-col justify-center items-center p-4 gap-y-5'>
+            <h1 className='text-2xl'>Añade una imagen de tu envío</h1>
+            <div className='p-10 border w-fit rounded-xl cursor-pointer' onClick={handleDivClick}>
+              <LuFolderInput size={70} />
+            </div>
+            <input
+              type="file"
+              ref={fileInputRef}
+              onChange={handleFileChange}
+              style={{ display: 'none' }}
+            />
+          </div>
+
           <Button
           variant={"ghost"}
           className="bg-pink text-white w-full p-3 m-3 rounded-xl font-bold text-lg mx-auto"
+          disabled={disabled}
           >
             Siguiente
           </Button>
         </form>
     )
 };
+
+export default SpecialProdModal;
