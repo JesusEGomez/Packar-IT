0425e08881243ab8530dc19cfcbe55bc4fad1631
gealtach <cleivaj93@gmail.com>
2024-01-18 12:38:41 -0300

    front viaje prod especial

--- src/app/components/sendProduct.tsx
+++ src/app/components/sendProduct.tsx
@@ -1,5 +1,5 @@
 import { Button } from "@/components/ui/button";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { TfiPackage } from "react-icons/tfi";
 import ProdForm from "./ProdForm";
 import { zodResolver } from "@hookform/resolvers/zod";
@@ -15,8 +15,19 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { IoMdArrowRoundBack } from "react-icons/io";
+import { GrBike } from "react-icons/gr";
+import { MdSurfing } from "react-icons/md";
+import { LuSofa, LuBedDouble } from "react-icons/lu";
+import { PiTelevisionBold } from "react-icons/pi";
+import { TbKayak } from "react-icons/tb";
+import { GiSkier } from "react-icons/gi";
+import { AiOutlineQuestionCircle } from "react-icons/ai";
 
 export const SendProduct = (props: any) => {
+  const specials:string[] = [];
+  const specialHandler = (item:string) => {
+    if(!specials.includes(item)) specials.push(item);
+  }
   const formSchema = z.object({
     pequeño: z.object({
       quantity: z.coerce.number(),
@@ -63,6 +74,7 @@ export const SendProduct = (props: any) => {
       props.setProductSelected(false);
     }
   }
+
 
   return (
     <div className="flex flex-col  p-4">
@@ -189,6 +201,62 @@ export const SendProduct = (props: any) => {
                   </FormItem>
                 )}
               ></FormField>
+            </div>
+          </div>
+          <div className="flex flex-col gap-y-4 my-4">
+            <h1 className="text-xl font-bold">Además, ¿Puedes transportar artículos especiales?</h1>
+            <p>Si las condiciones de tu vehículo y trayecto lo permiten, selecciona aquellos artículos que podrías transportar.</p>
+            <div className="flex flex-wrap gap-8 justify-center items-center">
+              <div className="flex flex-col gap-y-2 justify-center items-center">
+                <div 
+                className='p-5 rounded-full text-black bg-slate-300'
+                onClick={() => specialHandler('Bicicleta')}>
+                  <GrBike size={30} />
+                </div>
+                <p className="text-sm">Bicicleta</p>
+              </div>
+              <div className="flex flex-col gap-y-2 justify-center items-center" onClick={() => specialHandler('Tabla de surf')}>
+                <div className='p-5 rounded-full text-black bg-slate-300' >
+                  <MdSurfing size={30} />
+                </div>
+                <p className="text-sm">Tabla de surf</p>
+              </div>
+              <div className="flex flex-col gap-y-2 justify-center items-center" onClick={() => specialHandler('Silla')}>
+                <div className='p-5 rounded-full text-black bg-slate-300'>
+                  <LuSofa size={30} />
+                </div>
+                <p className="text-sm">Silla</p>
+              </div>
+              <div className="flex flex-col gap-y-2 justify-center items-center" onClick={() => specialHandler('Cama')}>
+                <div className='p-5 rounded-full text-black bg-slate-300'>
+                  <LuBedDouble size={30} />
+                </div>
+                <p className="text-sm">Cama</p>
+              </div>
+              <div className="flex flex-col gap-y-2 justify-center items-center" onClick={() => specialHandler('TV')}>
+                <div className='p-5 rounded-full text-black bg-slate-300'>
+                  <PiTelevisionBold size={30} />
+                </div>
+                <p className="text-sm">TV</p>
+              </div>
+              <div className="flex flex-col gap-y-2 justify-center items-center" onClick={() => specialHandler('Kayak')}>
+                <div className='p-5 rounded-full text-black bg-slate-300'>
+                  <TbKayak size={30} />
+                </div>
+                <p className="text-sm">Kayak</p>
+              </div>
+              <div className="flex flex-col gap-y-2 justify-center items-center" onClick={() => specialHandler('Esquis')}>
+                <div className='p-5 rounded-full text-black bg-slate-300'>
+                  <GiSkier size={30} />
+                </div>
+                <p className="text-sm">Esquíes</p>
+              </div>
+              <div className="flex flex-col gap-y-2 justify-center items-center" onClick={() => specialHandler('Otro')}>
+                <div className='p-5 rounded-full text-black bg-slate-300'>
+                  <AiOutlineQuestionCircle size={30} />
+                </div>
+                <p className="text-sm">Other</p>
+              </div>
             </div>
           </div>
           <Button
