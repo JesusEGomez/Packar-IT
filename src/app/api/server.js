78f20d853c5f7a24a55d457c4ab7dfdbdc6f7a7a
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2024-01-25 11:28:34 -0300

    ultima actualizacion

--- src/app/api/server.js
+++ src/app/api/server.js
@@ -54,25 +54,25 @@ io.on("connection", async (socket) => {
     console.log("A user disconnected:", socket.id);
   });
 
-  // Evento para aceptar un viaje y enviar notificación
-  socket.on("aceptar_viaje", async (data) => {
-    console.log("Solicitud de Viaje recibida:", data);
-
-    // Lógica para procesar la aceptación del viaje y enviar notificación.
+  socket.on("crear_envio", async (data) => {
+    console.log("Solicitud de envío recibida:", data);
+  
     if (userInfo) {
       const { userId, fullname } = userInfo;
-      console.log(`Viaje aceptado por ${fullname}`);
+      console.log(`Envío creado por ${fullname}`);
       // Aquí puedes agregar lógica adicional según las necesidades.
-      // Puedes enviar notificaciones, actualizar el estado del viaje, etc.
-      io.to(socket.id).emit("receive_notification", {
-        message: `Tu enviado ha sido a creado por ${fullname}.`,
+      // Puedes enviar notificaciones, actualizar el estado del envío, etc.
+      io.emit("receive_notification", {
+        message: `Tu envío ha sido creado por ${fullname}.`,
       });
-      console.log("Notificación enviada: Tu viaje ha sido aceptado.");
+      console.log("Notificación enviada: Tu envío ha sido creado.");
     } else {
-      console.log("Viaje aceptado por un usuario no autenticado");
+      console.log("Envío creado por un usuario no autenticado");
       // Puedes manejar la lógica para usuarios no autenticados según sea necesario.
+      // Por ejemplo, podrías enviar una notificación genérica o ignorar el evento.
     }
   });
+  
 });
 
 const PORT = 3001;
