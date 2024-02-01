eebdd8c3dd4f7a5b62b042ad7c403ed8045eec54
gealtach <cleivaj93@gmail.com>
2024-02-01 13:30:08 -0300

    if special

--- src/app/api/auth/viajes/route.ts
+++ src/app/api/auth/viajes/route.ts
@@ -51,33 +51,32 @@ export async function POST(request: RequestWithJson<ViajeRequest>) {
     special,
   } = await request.json();
 
-  // if (
-  //   !userId ||
-  //   !desde ||
-  //   !hasta ||
-  //   !cuando ||
-  //   !horaSalida ||
-  //   !horaLlegada ||
-  //   !precio ||
-  //   !envios ||
-  //   !special
-  // ) {
-  //   const missingFields = [];
+  if (
+    !userId ||
+    !desde ||
+    !hasta ||
+    !cuando ||
+    !horaSalida ||
+    !horaLlegada ||
+    !precio ||
+    !envios 
+  ) {
+    const missingFields = [];
 
-  //   if (!userId) missingFields.push("userId");
-  //   if (!desde) missingFields.push("desde");
-  //   if (!hasta) missingFields.push("hasta");
-  //   if (!cuando) missingFields.push("cuando");
-  //   if (!horaSalida) missingFields.push("horaSalida");
-  //   if (!horaLlegada) missingFields.push("horaLlegada");
-  //   if (!precio) missingFields.push("precio");
-  //   if (!envios) missingFields.push("envios");
+    if (!userId) missingFields.push("userId");
+    if (!desde) missingFields.push("desde");
+    if (!hasta) missingFields.push("hasta");
+    if (!cuando) missingFields.push("cuando");
+    if (!horaSalida) missingFields.push("horaSalida");
+    if (!horaLlegada) missingFields.push("horaLlegada");
+    if (!precio) missingFields.push("precio");
+    if (!envios) missingFields.push("envios");
 
-  //   return NextResponse.json(
-  //     { error: `Faltan campos obligatorios${missingFields.join(", ")}` },
-  //     { status: 400 }
-  //   );
-  // }
+    return NextResponse.json(
+      { error: `Faltan campos obligatorios${missingFields.join(", ")}` },
+      { status: 400 }
+    );
+  }
 
   try {
     const user = await User.findById(userId);
