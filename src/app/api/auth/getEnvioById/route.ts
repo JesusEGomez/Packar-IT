dee3993271cbdfc2ad4f9a053372d54056f9214e
gealtach <cleivaj93@gmail.com>
2024-02-06 20:12:04 -0300

    f

--- src/app/api/auth/getEnvioById/route.ts
+++ src/app/api/auth/getEnvioById/route.ts
@@ -2,6 +2,7 @@ import { connectDB } from "@/libs/mongodb";
 import Envio from "@/models/envios";
 import Producto from "@/models/productos";
 import { NextResponse } from "next/server";
+export const dynamic = "force-dynamic";
 
 export async function GET(request: Request) {
     try {
