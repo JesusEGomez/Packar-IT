1dbe06a58b429d0b743528d8b1e5147569c95ecc
JesusEGomez <jesus.emanuel.gomez77@gmail.com>
2024-02-07 18:03:19 -0300

    fix product

---
+++ src/app/loged/notifications/notEnvios/page.tsx
@@ -0,0 +1,9 @@
+const NotEnvios = () => {
+  return (
+    <div className="w-full flex  flex-col overflow-auto gap-2 justify-center items-center">
+      NotEnvios
+    </div>
+  );
+};
+
+export default NotEnvios;
---
+++ src/app/loged/notifications/notViajes/page.tsx
@@ -0,0 +1,9 @@
+const NotViajes = () => {
+  return (
+    <div className="w-full flex  flex-col overflow-auto gap-2 justify-center items-center">
+      NotViajes
+    </div>
+  );
+};
+
+export default NotViajes;
