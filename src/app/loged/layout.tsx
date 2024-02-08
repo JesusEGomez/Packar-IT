ba584ce92266b6252393277938744ccfa90f2b34
gealtach <cleivaj93@gmail.com>
2024-02-08 03:55:22 -0300

    permiso de notificaciones push

--- src/app/loged/layout.tsx
+++ src/app/loged/layout.tsx
@@ -9,6 +9,7 @@ import { useRouter } from "next/navigation";
 import ably from "@/app/api/ably/ably";
 import { connectDB } from "@/libs/mongodb";
 import Profile from "@/models/perfil";
+import { IoMdCloseCircleOutline } from "react-icons/io";
 
 function layout({ children }: React.PropsWithChildren) {
   const { data: session } = useSession();
@@ -17,7 +18,16 @@ function layout({ children }: React.PropsWithChildren) {
   const notificationChannelName = `notifications-${user._id}`;
   const [isSubscribed, setIsSubscribed] = useState(false);
   const navigation = useRouter();
+  const [popUpPermission, setPopUpPermission] = useState<boolean>(true);
+  const allowNotifications = () => {
+   Notification.requestPermission();
+   setPopUpPermission(false); 
+  }
+  const closePopUp = () => {
+    setPopUpPermission(false);
+  }
   useEffect(() => {
+    Notification.permission === 'granted' && setPopUpPermission(false);
     if (session?.user?.email) {
       fetchUser(session?.user?.email);
     }
@@ -43,6 +53,19 @@ function layout({ children }: React.PropsWithChildren) {
       <div className="  bottom-0">
         <BottmBar />
       </div>
+      {popUpPermission && (
+          <div className="fixed top-0 z-10 left-0 right-0 bottom-0 bg-black bg-opacity-50 flex items-center justify-center">
+            <div className="bg-slate-200 p-4 rounded-xl">
+              <div className="flex flex-col w-80 gap-y-3">
+                <button onClick={closePopUp} className="bg-pink text-white w-fit rounded-full p-2 hover:opacity-60"><IoMdCloseCircleOutline size={20} /></button>
+                <h1 className="font-bold w-72 mx-auto text-center">Hola, ¿nos autorizas a enviarte notificaciones para mantenerte al día sobre tus envios y viajes?</h1>
+                <button className="bg-pink w-full text-white font-bold rounded-xl my-2 p-3 hover:opacity-75"
+                onClick={allowNotifications}
+                >Permitir</button>
+              </div>
+            </div>
+          </div>
+        )}
     </div>
   );
 }
