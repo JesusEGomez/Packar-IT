43f755ce548c204635d798179a93787291bf713f
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2024-02-08 11:58:32 -0300

    Cambios perfil

--- src/app/loged/profile/page.tsx
+++ src/app/loged/profile/page.tsx
@@ -1,50 +1,18 @@
-"use client"
-
-
-import { Button } from "@/components/ui/button";
-import { FaEdit } from "react-icons/fa";
-import { zodResolver } from "@hookform/resolvers/zod";
-import { useForm } from "react-hook-form";
-import * as z from "zod";
+"use client";
 import { useState, useEffect } from "react";
 
-interface ProfileData {
-  nombre: string;
-  email: string;
-  telefono: string;
-  ciudad: string;
-  driverLicense: {
-    frontPhoto: string | null;
-    backPhoto: string | null;
-  };
-}
-
 function Profile() {
-  const [profileData, setProfileData] = useState<ProfileData | null>(null);
-  const formSchema = z.object({
-    nombre: z.string(),
-    email: z.string(),
-    telefono: z.string(),
-    ciudad: z.string(),
-    driverLicense: z.object({
-      frontPhoto: z.string().nullable(),
-      backPhoto: z.string().nullable(),
-    }),
-  });
-
-  const form = useForm<z.infer<typeof formSchema>>({
-    resolver: zodResolver(formSchema),
-    defaultValues: profileData ?? {},
-  });
+  const [profileData, setProfileData] = useState<any | null>(null);
+  const [viajesData, setViajesData] = useState<any | null>(null);
 
   useEffect(() => {
     async function fetchProfileData() {
       try {
         const response = await fetch(
-          `/api/auth/getProfileById/?id=65a6a4cf72c947b5cbb67646`
+          `/api/auth/getProfileById/?id=&{}`
         );
         const data = await response.json();
-        console.log(data);
+        console.log("Profile data:", data);
         setProfileData(data);
       } catch (error) {
         console.error("Error fetching profile data:", error);
@@ -53,50 +21,60 @@ function Profile() {
     fetchProfileData();
   }, []);
 
-  function onSubmit(values: z.infer<typeof formSchema>) {
-    console.log(values);
-  }
+  useEffect(() => {
+    async function fetchViajesData() {
+      try {
+        const response = await fetch(
+          `/api/auth/getTravelById/?id=65a6a4cf72c947b5cbb67646`
+        );
+        const data = await response.json();
+        console.log("Viajes data:", data);
+        setViajesData(data);
+      } catch (error) {
+        console.error("Error fetching travel data:", error);
+      }
+    }
+    fetchViajesData();
+  }, []);
 
   return (
-    <div className="container mx-auto h-screen flex flex-col justify-center items-center">
-      <h1 className="text-5xl font-bold mb-4">Mi perfil</h1>
-      <div className="max-w-md p-8 bg-white rounded-md shadow-md">
-        {profileData && (
-          <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col space-y-4">
-            <div>
-              <label>Nombre:</label>
-              <span>{profileData.nombre}</span>
-            </div>
-            <div>
-              <label>Email:</label>
-              <span>{profileData.email}</span>
-            </div>
-            <div>
-              <label>Teléfono:</label>
-              <span>{profileData.telefono}</span>
-            </div>
-            <div>
-              <label>Ciudad:</label>
-              <span>{profileData.ciudad}</span>
-            </div>
-            {/* Si profileData.driverLicense es un objeto */}
-            {profileData.driverLicense && (
-              <>
+    <div className="min-h-screen bg-gray-100 py-6 flex flex-col justify-center sm:p-18">
+      <h1 className="text-4xl font-bold mb-4 text-gray-800 text-center">
+        Mi perfil
+      </h1>
+      <div className="relative py-3 sm:max-w-xl sm:mx-auto">
+        <div className="absolute inset-0 bg-gradient-to-r from-pink-500 to-rose-500 shadow-lg transform -skew-y-6 sm:skew-y-0 sm:-rotate-6 sm:rounded-3xl"></div>
+        <div className="relative px-4 py-10 bg-white shadow-lg sm:rounded-3xl sm:p-20">
+          <h2 className="text-gray-800 text-xl text-center font-bold mb-6">
+            Datos Personales
+          </h2>
+          <section className="mb-8">
+            {profileData && (
+              <div className="grid grid-cols-1 gap-6 md:grid-cols-2">
                 <div>
-                  <label>Foto delantera de la licencia de conducir:</label>
-                  <span>{profileData.driverLicense.frontPhoto}</span>
+                  <p className="text-gray-700 text-lg mb-4">
+                    <span className="font-bold">Nombre:</span>{" "}
+                    {profileData.fullName}
+                  </p>
+                  <p className="text-gray-700 text-lg mb-4">
+                    <span className="font-bold">Email:</span>{" "}
+                    {profileData.email}
+                  </p>
                 </div>
                 <div>
-                  <label>Foto trasera de la licencia de conducir:</label>
-                  <span>{profileData.driverLicense.backPhoto}</span>
+                  <p className="text-gray-700 text-lg mb-4">
+                    <span className="font-bold">Teléfono:</span>{" "}
+                    {profileData.phoneNumber}
+                  </p>
+                  <p className="text-gray-700 text-lg mb-4">
+                    <span className="font-bold">Ciudad:</span>{" "}
+                    {profileData.city}
+                  </p>
                 </div>
-              </>
+              </div>
             )}
-            <Button className="bg-pink py-2 rounded-md" type="submit">
-              <FaEdit className="mr-2" /> Modificar
-            </Button>
-          </form>
-        )}
+          </section>
+        </div>
       </div>
     </div>
   );
