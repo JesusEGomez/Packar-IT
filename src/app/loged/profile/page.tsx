6c43d1589a3dfe8a15d0c2dba4dd12500f127cc8
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2024-02-09 13:15:29 -0300

    Api

--- src/app/loged/profile/page.tsx
+++ src/app/loged/profile/page.tsx
@@ -6,13 +6,42 @@ import { useSession } from "next-auth/react";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 function Profile() {
+  const [profileData, setProfileData] = useState<any | null>(null);
   const { fetchUser } = useUserState((state) => state);
   const { data: session } = useSession();
+  const [userId, setUserId] = useState<string | null>(null);
 
   console.log(session);
 
   useEffect(() => {
     fetchUser(session?.user?.email!);
+  }, []);
+
+  useEffect(() => {
+    async function fetchProfileData() {
+      try {
+        const responseUser = await fetch(
+          `/api/auth/myid/?email=${session?.user?.email}`,
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
+        const userData = await responseUser.json();
+        setUserId(userData);
+        const response = await fetch(
+          `/api/auth/getProfileById/?id=${userData._id}`
+        );
+        const data = await response.json();
+        console.log("Profile data:", data);
+        setProfileData(data);
+      } catch (error) {
+        console.error("Error fetching profile data:", error);
+      }
+    }
+    fetchProfileData();
   }, []);
 
   if (!session) {
@@ -27,11 +56,11 @@ function Profile() {
         </h1>
       </div>
       <div className="flex justify-center items-center m-3">
-      <dl className="border-t border-gray-200 px-4 py-5 sm:p-0 m-5">
-        <Avatar className="w-20 h-20 mr-2">
-          <AvatarImage src={session?.user?.image!} alt="@shadcn" />
-          <AvatarFallback>?</AvatarFallback>
-        </Avatar>
+        <dl className="border-t border-gray-200 px-4 py-5 sm:p-0 m-5">
+          <Avatar className="w-20 h-20 mr-2">
+            <AvatarImage src={session?.user?.image!} alt="@shadcn" />
+            <AvatarFallback>?</AvatarFallback>
+          </Avatar>
         </dl>
       </div>
       <dl className="border-t border-gray-200 px-4 py-5 sm:p-0 m-5">
@@ -39,25 +68,25 @@ function Profile() {
           <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt className="text-sm font-medium text-gray-700">Nombre</dt>
             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-            {session?.user?.name!}
+              {session?.user?.name!}
             </dd>
           </div>
           <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt className="text-sm font-medium text-gray-700">Email</dt>
             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-            {session?.user?.email!}
+              {session?.user?.email!}
             </dd>
           </div>
           <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt className="text-sm font-medium text-gray-700">Ciudad</dt>
             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              Mar del Plata
+              {profileData?.city || 'No disponible'}
             </dd>
           </div>
           <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
             <dt className="text-sm font-medium text-gray-700">Teléfono</dt>
             <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              (123) 456-7890
+            {profileData?.phoneNumber || 'No disponible'}
             </dd>
           </div>
         </div>
