3ba160ab4d8ab98c14b15c282d3b1ebbf211ca54
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2024-02-15 19:31:06 -0300

    Input telefono solo numeros

--- src/app/loged/profile/page.tsx
+++ src/app/loged/profile/page.tsx
@@ -7,14 +7,14 @@ import { AiOutlineEdit } from "react-icons/ai";
 
 function Profile() {
   const [profileData, setProfileData] = useState<any | null>(null);
-  const { fetchUser , user } = useUserState((state) => state);
+  const { fetchUser, user } = useUserState((state) => state);
   const { data: session } = useSession();
   const [userId, setUserId] = useState<string | null>(null);
   const [phoneNumber, setPhoneNumber] = useState<string>("");
   const [city, setCity] = useState<string>("");
-  const [phoneNumberEditMode, setPhoneNumberEditMode] = useState<boolean>(false);
+  const [phoneNumberEditMode, setPhoneNumberEditMode] =
+    useState<boolean>(false);
   const [cityEditMode, setCityEditMode] = useState<boolean>(false);
- 
 
   useEffect(() => {
     fetchUser(session?.user?.email!);
@@ -27,9 +27,9 @@ function Profile() {
           console.error("El ID del usuario no está definido.");
           return;
         }
-        const data = await (await fetch(
-          `/api/auth/getProfileById/?id=${user._id}`
-        )).json();
+        const data = await (
+          await fetch(`/api/auth/getProfileById/?id=${user._id}`)
+        ).json();
         console.log("Profile data:", data);
         setProfileData(data);
         setPhoneNumber(data?.phoneNumber || "");
@@ -40,10 +40,20 @@ function Profile() {
     }
     fetchProfileData();
   }, [user]);
-
+  const handlePhoneNumberChange = (e : any) => {
+    const inputPhoneNumber = e.target.value;
+    if (/^\d+$/.test(inputPhoneNumber) || inputPhoneNumber === "") {
+      setPhoneNumber(inputPhoneNumber);
+    }
+  };
   const handleUpdateProfile = async () => {
     try {
-      const response = await fetch( `/api/auth/getProfileById/?id=${user._id}`, {
+      if (!phoneNumber || !city) {
+        console.error("El teléfono y la ciudad no pueden estar vacíos.");
+        return;
+      }
+
+      const response = await fetch(`/api/auth/getProfileById/?id=${user._id}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
@@ -76,84 +86,84 @@ function Profile() {
 
   return (
     <>
-    <div className="rounded-lg shadow overflow-hidden bg-pink-50">
-      <div className="px-4 py-5 sm:px-6 text-white">
-        <h1 className="text-4xl leading-6 font-bold text-gray-900 text-center">
-          Mi perfil
-        </h1>
-      </div>
-      
-      <dl className="border-t border-gray-200 px-4 py-5 sm:p-0 m-5">
-        <div className="sm:divide-y sm:divide-gray-200">
-          <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-700">Nombre</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              {session?.user?.name!}
-            </dd>
-          </div>
-          <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-            <dt className="text-sm font-medium text-gray-700">Email</dt>
-            <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
-              {session?.user?.email!}
-            </dd>
-          </div>
-          <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-  <dt className="text-sm font-medium text-gray-700">Ciudad</dt>
-  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 relative">
-    {cityEditMode ? (
-      <input
-        type="text"
-        value={city}
-        onChange={(e) => setCity(e.target.value)}
-        onBlur={() => setCityEditMode(false)}
-        className="w-full border rounded-md px-2 py-1"
-        autoFocus
-      />
-    ) : (
-      <>
-        <span>{city}</span>
-        <button
-          onClick={() => setCityEditMode(true)}
-          className="absolute top-0 right-0 p-1"
-        >
-          <AiOutlineEdit />
-          <span className="sr-only">Editar Ciudad</span>
-        </button>
-      </>
-    )}
-  </dd>
-</div>
-<div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
-  <dt className="text-sm font-medium text-gray-700">Teléfono</dt>
-  <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 relative">
-    {phoneNumberEditMode ? (
-      <input
-        type="text"
-        value={phoneNumber || ""}
-        onChange={(e) => setPhoneNumber(e.target.value)}
-        onBlur={() => setPhoneNumberEditMode(false)}
-        className="w-full border rounded-md px-2 py-1"
-        autoFocus
-      />
-    ) : (
-      <>
-        <span>{phoneNumber}</span>
-        <button
-          onClick={() => setPhoneNumberEditMode(true)}
-          className="absolute top-0 right-0 p-1"
-        >
-          <AiOutlineEdit />
-          <span className="sr-only">Editar Teléfono</span>
-        </button>
-      </>
-    )}
-  </dd>
-</div>
+      <div className="rounded-lg shadow overflow-hidden bg-pink-50">
+        <div className="px-4 py-5 sm:px-6 text-white">
+          <h1 className="text-4xl leading-6 font-bold text-gray-900 text-center">
+            Mi perfil
+          </h1>
         </div>
-      </dl>
 
-      {/* Botón para guardar los cambios */}
-    </div>
+        <dl className="border-t border-gray-200 px-4 py-5 sm:p-0 m-5">
+          <div className="sm:divide-y sm:divide-gray-200">
+            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-700">Nombre</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {session?.user?.name!}
+              </dd>
+            </div>
+            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-700">Email</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2">
+                {session?.user?.email!}
+              </dd>
+            </div>
+            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-700">Ciudad</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 relative">
+                {cityEditMode ? (
+                  <input
+                    type="text"
+                    value={city}
+                    onChange={(e) => setCity(e.target.value)}
+                    onBlur={() => setCityEditMode(false)}
+                    className="w-full border rounded-md px-2 py-1"
+                    autoFocus
+                  />
+                ) : (
+                  <>
+                    <span>{city}</span>
+                    <button
+                      onClick={() => setCityEditMode(true)}
+                      className="absolute top-0 right-0 p-1"
+                    >
+                      <AiOutlineEdit />
+                      <span className="sr-only">Editar Ciudad</span>
+                    </button>
+                  </>
+                )}
+              </dd>
+            </div>
+            <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
+              <dt className="text-sm font-medium text-gray-700">Teléfono</dt>
+              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 relative">
+                {phoneNumberEditMode ? (
+                  <input
+                    type="text"
+                    value={phoneNumber || ""}
+                    onChange={handlePhoneNumberChange}
+                    onBlur={() => setPhoneNumberEditMode(false)}
+                    className="w-full border rounded-md px-2 py-1"
+                    autoFocus
+                  />
+                ) : (
+                  <>
+                    <span>{phoneNumber}</span>
+                    <button
+                      onClick={() => setPhoneNumberEditMode(true)}
+                      className="absolute top-0 right-0 p-1"
+                    >
+                      <AiOutlineEdit />
+                      <span className="sr-only">Editar Teléfono</span>
+                    </button>
+                  </>
+                )}
+              </dd>
+            </div>
+          </div>
+        </dl>
+
+        {/* Botón para guardar los cambios */}
+      </div>
       <div className="flex justify-center mt-4">
         <button
           onClick={handleUpdateProfile}
@@ -162,7 +172,7 @@ function Profile() {
           Guardar cambios
         </button>
       </div>
-      </>
+    </>
   );
 }
 
