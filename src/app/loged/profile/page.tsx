a6cf76cbd1a8425a6c861838f5dac41fdde7d7ac
Mauroo Ciappina <ciappinamaurooj@gmail.com>
2024-02-16 10:13:29 -0300

    Mensaje de error bajo el input de teléfono y ciudad

--- src/app/loged/profile/page.tsx
+++ src/app/loged/profile/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 import { useState, useEffect } from "react";
 import loading from "../../../app/loading";
 import useUserState from "../../../app/store/sotre";
@@ -12,7 +12,8 @@ function Profile() {
   const [userId, setUserId] = useState<string | null>(null);
   const [phoneNumber, setPhoneNumber] = useState<string>("");
   const [city, setCity] = useState<string>("");
-  const [phoneNumberEditMode, setPhoneNumberEditMode] = useState<boolean>(false);
+  const [phoneNumberEditMode, setPhoneNumberEditMode] =
+    useState<boolean>(false);
   const [cityEditMode, setCityEditMode] = useState<boolean>(false);
   const [phoneNumberError, setPhoneNumberError] = useState<string>("");
   const [cityError, setCityError] = useState<string>("");
@@ -45,11 +46,21 @@ function Profile() {
 
   const handlePhoneNumberChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     const inputPhoneNumber = e.target.value;
-    if (/^\d+$/.test(inputPhoneNumber) || inputPhoneNumber === "") {
-      setPhoneNumber(inputPhoneNumber);
-      setPhoneNumberError("");
+    setPhoneNumber(inputPhoneNumber);
+    if (!inputPhoneNumber.trim()) {
+      setPhoneNumberError("El teléfono no puede estar vacío.");
     } else {
-      setPhoneNumberError("El teléfono solo puede contener números.");
+      setPhoneNumberError("");
+    }
+  };
+
+  const handleCityChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const inputCity = e.target.value;
+    setCity(inputCity);
+    if (!inputCity.trim()) {
+      setCityError("La ciudad no puede estar vacía.");
+    } else {
+      setCityError("");
     }
   };
 
@@ -57,18 +68,21 @@ function Profile() {
     try {
       if (!phoneNumber) {
         setPhoneNumberError("El teléfono no puede estar vacío.");
-        return;
       } else {
         setPhoneNumberError("");
       }
       if (!city) {
         setCityError("La ciudad no puede estar vacía.");
-        return;
       } else {
         setCityError("");
       }
 
-      const response = await fetch(`/api/auth/getProfileById/?id=${user?._id}`, {
+      if (!phoneNumber || !city) {
+        console.error("El teléfono y la ciudad no pueden estar vacíos.");
+        return;
+      }
+
+      const response = await fetch(`/api/auth/getProfileById/?id=${user._id}`, {
         method: "PUT",
         headers: {
           "Content-Type": "application/json",
@@ -87,7 +101,7 @@ function Profile() {
           phoneNumber,
           city,
         });
-        setSuccessMessage("El perfil se ha actualizado correctamente.");
+        setSuccessMessage("Perfil actualizado con éxito");
         setTimeout(() => {
           setSuccessMessage("");
         }, 3000);
@@ -134,17 +148,17 @@ function Profile() {
                     <input
                       type="text"
                       value={city}
-                      onChange={(e) => setCity(e.target.value)}
+                      onChange={handleCityChange}
                       onBlur={() => setCityEditMode(false)}
-                      className={"w-full border rounded-md px-2 py-1 focus:border-pink-500"}
+                      className={`w-full border rounded-md px-2 py-1 ${
+                        cityError ? "border-red-500" : ""
+                      }`}
                       autoFocus
                     />
-                    {cityError && (
-                      <p className="text-red-500">{cityError}</p>
-                    )}
                   </>
                 ) : (
                   <>
+                    {cityError && <p className="text-red-500">{cityError}</p>}
                     <span>{city}</span>
                     <button
                       onClick={() => setCityEditMode(true)}
@@ -159,24 +173,25 @@ function Profile() {
             </div>
             <div className="py-3 sm:py-5 sm:grid sm:grid-cols-3 sm:gap-4 sm:px-6">
               <dt className="text-sm font-medium text-gray-700">Teléfono</dt>
-              <dd className="mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 relative">
+              <dd className=" mt-1 text-sm text-gray-900 sm:mt-0 sm:col-span-2 relative">
                 {phoneNumberEditMode ? (
                   <>
                     <input
                       type="text"
-                      value={phoneNumber || ""}
+                      value={phoneNumber}
                       onChange={handlePhoneNumberChange}
                       onBlur={() => setPhoneNumberEditMode(false)}
-                      className={"w-full border rounded-md px-2 py-1 focus:border-pink-500"}
+                      className={`w-full border rounded-md px-2 py-1 ${
+                        phoneNumberError ? "border-red-500" : ""
+                      }`}
                       autoFocus
                     />
-                    {phoneNumberError && (
-                      <p className="text-red-500">{phoneNumberError}</p>
-                    )}
                   </>
                 ) : (
                   <>
-                    <span>{phoneNumber}</span>
+                  {phoneNumberError && (
+                    <p className="text-red-500">{phoneNumberError}</p>
+                  )}
                     <button
                       onClick={() => setPhoneNumberEditMode(true)}
                       className="absolute top-0 right-0 p-1"
@@ -184,19 +199,13 @@ function Profile() {
                       <AiOutlineEdit />
                       <span className="sr-only">Editar Teléfono</span>
                     </button>
+                    <span>{phoneNumber}</span>
                   </>
                 )}
               </dd>
             </div>
           </div>
         </dl>
-
-        {/* Mensaje de éxito */}
-        {successMessage && (
-          <div className="text-green-500 text-center">
-            {successMessage}
-          </div>
-        )}
 
         {/* Botón para guardar los cambios */}
       </div>
@@ -208,6 +217,9 @@ function Profile() {
           Guardar cambios
         </button>
       </div>
+      {successMessage && (
+        <div className="text-green-500 m-2 text-center">{successMessage}</div>
+      )}
     </>
   );
 }
