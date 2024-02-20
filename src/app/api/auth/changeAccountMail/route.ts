e8069a0e7c9d1051bd25aa2a85eaf540d810c5d3
JesusEGomez <jesus.emanuel.gomez77@gmail.com>
2024-02-20 09:18:50 -0300

    fix

--- src/app/api/auth/changeAccountMail/route.ts
+++ src/app/api/auth/changeAccountMail/route.ts
@@ -1,6 +1,5 @@
 import transporter from "../../../../libs/nodemailer.config";
 import { NextResponse } from "next/server";
-export const dynamic = "force-dynamic";
 
 export async function POST(request: Request) {
   try {
