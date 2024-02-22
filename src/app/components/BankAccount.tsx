1bd7a5424cc13111f83990294fe57d30a0976ae7
JesusEGomez <jesus.emanuel.gomez77@gmail.com>
2024-02-21 21:38:32 -0300

    update with main

--- src/app/components/BankAccount.tsx
+++ src/app/components/BankAccount.tsx
@@ -92,65 +92,6 @@ const BankAccount = (props: any) => {
       accountNumber: data.accountNumber,
       userId: parsedUser._id,
       email: parsedUser.email,
-      idDoc: idData,
-    };
-    if (data) {
-      setLoading(true);
-      try {
-        const createAccount = await fetch("/api/auth/pagoDriver", {
-          headers: {
-            "Content-Type": "application/json",
-          },
-          method: "POST",
-          body: JSON.stringify(accountData),
-        });
-        console.log(createAccount);
-        createAccount.ok && setCuentaEnviada(true);
-        await fetch("/api/auth/newAccountMail", {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify(accountData),
-        });
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-  };
-
-  const closeIdModal = () => {
-    setIsIdModalOpen(false);
-    setLoadFetch(!loadFetch);
-  };
-
-  const close = () => {
-    setCuentaEnviada(false);
-    props.closeAccount();
-  };
-  const {
-    register,
-    handleSubmit,
-    formState: { errors },
-  } = useForm<FormData>();
-
-  const onSubmit: SubmitHandler<FormData> = async (data) => {
-    const accountData = {
-      country: data.countries,
-      bank: data.bank,
-      name: data.name,
-      lastName: data.lastName,
-      phone: data.phonecode + data.phone,
-      address: data.address,
-      city: data.city,
-      zipCode: data.zipCode,
-      dd: data.dd,
-      mm: data.mm,
-      aaaa: data.aaaa,
-      accountNumber: data.accountNumber,
-      userId: parsedUser._id,
-      email: parsedUser.email,
       idDoc: `${idData.frontPhoto}, ${idData.backPhoto}, ${idData.number}, ${idData.type}`,
     };
     if (data) {
@@ -264,37 +205,228 @@ const BankAccount = (props: any) => {
         </div>
       ) : (
         <div>
+          <h1 className="text-xl my-4 pb-4 border-b">
+            Ingresa los datos de tu cuenta para recibir tus pagos
+          </h1>
           <select
-            id="phonecode"
-            {...register("phonecode", {
+            {...register("countries", {
               required: { value: true, message: "Campo requerido" },
             })}
+            id="countries"
             className={`p-2 rounded bg-white w-full ${
               errors.bank ? "border-red-500" : ""
             }`}
           >
             <option value="" disabled selected>
-              Selecciona el código de tu país
+              Selecciona tu país
             </option>
             {countries &&
               countries.map((country, i) => (
                 <option
+                  value={country.cca2}
                   key={i}
-                  value={`${country.idd.root}${country.idd.suffixes[0]}`}
-                >{`${country.flag} ${country.translations.spa.common} (${country.idd.root}${country.idd.suffixes[0]})`}</option>
+                >{`${country.translations.spa.common} (${country.cca2})`}</option>
               ))}
           </select>
-          <input
-            {...register("phone", {
-              required: { value: true, message: "Campo requerido" },
-            })}
-            type="text"
-            id="phone"
-            placeholder="12345678"
-            className={`p-2 rounded bg-white w-full ${
-              errors.bank ? "border-red-500" : ""
-            }`}
-          />
+          <div className="flex flex-col w-full gap-y-2">
+            <label htmlFor="">Ingresa tu banco:</label>
+            <input
+              {...register("bank", {
+                required: { value: true, message: "Campo requerido" },
+              })}
+              type="text"
+              id="bank"
+              placeholder="Banco de bilbao"
+              className={`p-2 rounded bg-white w-full ${
+                errors.bank ? "border-red-500" : ""
+              }`}
+            />
+          </div>
+          <div className="flex flex-col w-full gap-y-2">
+            <label htmlFor="">Ingresa tu nro de cuenta:</label>
+            <input
+              {...register("accountNumber", {
+                required: { value: true, message: "Campo requerido" },
+              })}
+              type="text"
+              id="accountNumber"
+              placeholder="12345678"
+              className={`p-2 rounded bg-white w-full ${
+                errors.bank ? "border-red-500" : ""
+              }`}
+            />
+          </div>
+          <div className="flex flex-col w-full gap-y-2">
+            <label htmlFor="">Ingresa tu nombre:</label>
+            <input
+              {...register("name", {
+                required: { value: true, message: "Campo requerido" },
+              })}
+              type="text"
+              id="name"
+              placeholder="John"
+              className={`p-2 rounded bg-white w-full ${
+                errors.bank ? "border-red-500" : ""
+              }`}
+            />
+          </div>
+          <div className="flex flex-col w-full gap-y-2">
+            <label htmlFor="">Ingresa tu apellido:</label>
+            <input
+              {...register("lastName", {
+                required: { value: true, message: "Campo requerido" },
+              })}
+              type="text"
+              id="lastName"
+              placeholder="Doe"
+              className={`p-2 rounded bg-white w-full ${
+                errors.bank ? "border-red-500" : ""
+              }`}
+            />
+          </div>
+          <div className="flex flex-col w-full gap-y-2">
+            <label htmlFor="">Ingresa tu teléfono:</label>
+            <div>
+              <select
+                id="phonecode"
+                {...register("phonecode", {
+                  required: { value: true, message: "Campo requerido" },
+                })}
+                className={`p-2 rounded bg-white w-full ${
+                  errors.bank ? "border-red-500" : ""
+                }`}
+              >
+                <option value="" disabled selected>
+                  Selecciona el código de tu país
+                </option>
+                {countries &&
+                  countries.map((country, i) => (
+                    <option
+                      key={i}
+                      value={`${country.idd.root}${country.idd.suffixes[0]}`}
+                    >{`${country.flag} ${country.translations.spa.common} (${country.idd.root}${country.idd.suffixes[0]})`}</option>
+                  ))}
+              </select>
+              <input
+                {...register("phone", {
+                  required: { value: true, message: "Campo requerido" },
+                })}
+                type="text"
+                id="phone"
+                placeholder="12345678"
+                className={`p-2 rounded bg-white w-full ${
+                  errors.bank ? "border-red-500" : ""
+                }`}
+              />
+            </div>
+          </div>
+          <div className="flex flex-col w-full gap-y-2">
+            <label htmlFor="">Ingresa tu ciudad:</label>
+            <input
+              {...register("city", {
+                required: { value: true, message: "Campo requerido" },
+              })}
+              type="text"
+              id="city"
+              placeholder="Barcelona"
+              className={`p-2 rounded bg-white w-full ${
+                errors.bank ? "border-red-500" : ""
+              }`}
+            />
+          </div>
+          <div className="flex flex-col w-full gap-y-2">
+            <label htmlFor="">Ingresa tu dirección:</label>
+            <input
+              {...register("address", {
+                required: { value: true, message: "Campo requerido" },
+              })}
+              type="text"
+              id="address"
+              placeholder="Carrer de la Ciutat"
+              className={`p-2 rounded bg-white w-full ${
+                errors.bank ? "border-red-500" : ""
+              }`}
+            />
+          </div>
+          <div className="flex flex-col w-full gap-y-2">
+            <label htmlFor="">Ingresa tu codigo postal:</label>
+            <input
+              {...register("zipCode", {
+                minLength: 5,
+                required: { value: true, message: "Campo requerido" },
+              })}
+              type="text"
+              id="zipCode"
+              placeholder="12345"
+              className={`p-2 rounded bg-white w-full ${
+                errors.bank ? "border-red-500" : ""
+              }`}
+            />
+          </div>
+          <div className="flex flex-col w-full gap-y-2">
+            <label htmlFor="">Ingresa tu fecha de nacimiento:</label>
+            <div className="flex gap-x-3">
+              <input
+                {...register("dd", {
+                  min: 1,
+                  max: 31,
+                  required: { value: true, message: "Campo requerido" },
+                })}
+                type="text"
+                id="dd"
+                placeholder="DD"
+                className={`p-2 rounded bg-white w-20 ${
+                  errors.bank ? "border-red-500" : ""
+                }`}
+              />
+              <input
+                {...register("mm", {
+                  min: 1,
+                  max: 12,
+                  required: { value: true, message: "Campo requerido" },
+                })}
+                type="text"
+                id="mm"
+                placeholder="MM"
+                className={`p-2 rounded bg-white w-20 ${
+                  errors.bank ? "border-red-500" : ""
+                }`}
+              />
+              <input
+                {...register("aaaa", {
+                  min: 1990,
+                  max: 2024,
+                  required: { value: true, message: "Campo requerido" },
+                })}
+                type="text"
+                id="aaaa"
+                placeholder="AAAA"
+                className={`p-2 rounded bg-white w-20 ${
+                  errors.bank ? "border-red-500" : ""
+                }`}
+              />
+            </div>
+          </div>
+          {profile?.idDocument?.isLoaded ? (
+            <Button
+              variant={"ghost"}
+              className="bg-pink text-white w-full p-3 m-3 rounded-xl font-bold text-lg mx-auto"
+            >
+              {loading ? (
+                <Loader2 className="mr-2 h-4 w-4 animate-spin" />
+              ) : (
+                "Enviar"
+              )}
+            </Button>
+          ) : (
+            <Button
+              variant={"ghost"}
+              className="bg-pink text-white w-full p-3 m-3 rounded-xl font-bold text-lg mx-auto"
+              onClick={() => setIsIdModalOpen(true)}
+            >
+              Carga tu documento de Identidad
+            </Button>
+          )}
         </div>
       )}
 
