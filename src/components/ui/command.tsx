b37393242da63a0a7d4a71c32212c4313adaa7f2
Julian Kisner <julikisner@gmail.com>
2024-02-22 12:04:14 -0300

    height

--- src/components/ui/command.tsx
+++ src/components/ui/command.tsx
@@ -16,7 +16,7 @@ const Command = React.forwardRef<
     ref={ref}
     style={{ height: 'calc(100vh - 63px)' }}
     className={cn(
-      "flex w-full flex-col overflow-hidden rounded-md bg-white text-slate-950 dark:bg-slate-950 dark:text-slate-50",
+      "flex h-full w-full flex-col overflow-hidden rounded-md bg-white text-slate-950 dark:bg-slate-950 dark:text-slate-50",
       className
     )}
     {...props}
